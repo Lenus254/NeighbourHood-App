fac4895dbe5d035fcfe6c5aa29b2820d38bf982c
lenus254 <linus.cheruiyot@student.moringaschool.com>
2022-03-21 19:06:36 +0300

    added new area view function to app

---
+++ area/forms.py
@@ -0,0 +1,26 @@
+from django import forms
+from django.contrib.auth.models import User
+from .models import Profile, Posts, Business, Area
+
+
+class EditProfileForm(forms.ModelForm):
+  class Meta:
+    model = Profile
+    exclude = ('user', 'location')
+    
+class AreaForm(forms.ModelForm):
+    class Meta:
+        model = Area
+        exclude = ('admin',)
+
+class BusinessForm(forms.ModelForm):
+  class Meta:
+    model = Business
+    exclude = ('owner', 'hood')
+
+
+
+class PostForm(forms.ModelForm):
+    class Meta:
+        model = Posts
+        exclude = ('user', 'hood')
--- area/views.py
+++ area/views.py
@@ -1,7 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
-
-from area.models import Area
+from .forms import EditProfileForm, AreaForm, BusinessForm, PostForm
+from area.models import Area, Profile, Posts, Business
 
 # Create your views here.
 @login_required(login_url='login')
@@ -11,3 +11,15 @@ def index(request):
         "hoods" : neighbourhoods
     }
     return render(request, 'index.html', ctx)
+
+def new_hood(request):
+    if request.method == 'POST':
+        form = AreaForm(request.POST, request.FILES)
+        if form.is_valid():
+            hood = form.save(commit=False)
+            hood.admin = request.user.profile
+            hood.save()
+            return redirect('index')
+    else:
+        form = AreaForm()
+    return render(request, 'create_hood.html', {'form': form})
